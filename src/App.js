32e14b1b23024afa17d9eae63d99745849c0ae85
Daniel <danielkofi4@gmail.com>
2022-01-24 17:37:39 +0000

    chat-engine integrated

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import './App.css';
+import { ChatEngine } from 'react-chat-engine';
 
 const App = () => {
-  return <div></div>;
+  return <ChatEngine height="100vh" projectID="4c0e7056-3510-4e64-8fa2-2bc0f54f8151" userName="dyofficial" userSecret="123123" />;
 };
 
 export default App;
