069147f30c85bb0109914427c78618e27b792194
Daniel <danielkofi4@gmail.com>
2022-01-25 11:29:26 +0000

    chat feed component created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import './App.css';
 import { ChatEngine } from 'react-chat-engine';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
-  return <ChatEngine height="100vh" projectID="4c0e7056-3510-4e64-8fa2-2bc0f54f8151" userName="dyofficial" userSecret="123123" />;
+  return <ChatEngine height="100vh" projectID="4c0e7056-3510-4e64-8fa2-2bc0f54f8151" userName="dyofficial" userSecret="123123" renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />} />;
 };
 
 export default App;
